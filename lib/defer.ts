645b57f4a91c41202262beeadf5e366d2f287c76
Tony Gaetani <tony.gaetani@gmail.com>
2017-10-14 18:41:52 -0500

    fix: add jsdoc

--- lib/defer.ts
+++ lib/defer.ts
@@ -1,6 +1,20 @@
 import * as Rx from 'rxjs';
+import * as R from 'ramda';
 
-export function safeDefer(handler: () => any, log: {error: (data: any, msg: string) => any}) {
+declare type Logger = {error: (data: any, msg: string) => any};
+
+/**
+ * Returns an observable that will swallow (and optionally log) all errors thrown by a handler
+ * and return either an empty Observable when an error occurs or the result of the handler
+ *
+ * @param {Function} handler: a function that is called and any errors thrown are safely swallowed
+ * @param {Logger} log: (optional) a logger to log any errors
+ * @return {Observable<any>} an empty Observable when an error is thrown by the handler, otherwise
+ * 							 the result of the handler function
+ * @method safeDefer
+ * @type {R.CurriedTypeGuard2<() => any, Logger, T2>}
+ */
+export const safeDefer = R.curry((handler: () => any, log: Logger) => {
 	return Rx.Observable.defer(handler)
 		.catch((err: Error, result: any) => {
 			if (log && log.error) {
@@ -9,4 +23,4 @@ export function safeDefer(handler: () => any, log: {error: (data: any, msg: stri
 
 			return Rx.Observable.empty();
 		});
-}
+});
--- lib/retry.ts
+++ lib/retry.ts
@@ -1,6 +1,16 @@
 import * as R from 'ramda';
 import * as Rx from 'rxjs';
 
+/**
+ * Returns an Observable that resubscribes to the source observable each time it emits
+ * an error until a limit (max) is reached and then emits that error
+ *
+ * @param {number} max: the number of errors to allow before emitting an error
+ * @param {Observable<any>} error$: an Observable that is re-subscribed to on error up to the limit
+ * @return {Observable<number>} an Observable of the number of retries
+ * @type {R.CurriedFunction2<number, Observable<any>, Observable<any>>}
+ * @method maxAttempts
+ */
 export const maxAttempts = R.curry((max: number, error$: Rx.Observable<any>) => {
 		return error$.scan(function (errorCount: number, err: Error) {
 			if (errorCount >= max - 1) {
@@ -11,18 +21,36 @@ export const maxAttempts = R.curry((max: number, error$: Rx.Observable<any>) => 
 	}
 );
 
+/**
+ * Returns an observable that resubscribes to the source observable each time it emits
+ * an error
+ *
+ * @param {Observable<any>} error$: an Observable that is re-subscribed to on error
+ * @return {Observable<number>} an Observable of the number of retries
+ * @method forever
+ */
 export const forever = (error$: Rx.Observable<any>) => {
 	return error$.scan((errorCount: number) => {
 		return errorCount + 1;
 	}, 0);
 };
 
+/**
+ * Returns an observable that never resubscribes to the source observable
+ *
+ * @param {Observable<any>} error$: an Observable that is never re-subscribed to on error
+ * @return {Observable<Error>} an Observable of any error that was thrown by the source observable
+ * @method forever
+ */
 export const never = (error$: Rx.Observable<any>) => {
 	return error$.map((err: Error) => {
 		throw err;
 	});
 };
 
+/**
+ * Typeof retry Observables
+ */
 export interface RetryPolicy {
 	(errors: Rx.Observable<any>): Rx.Observable<number>;
 }
