e064ad1746efe8b34d2ab40db068429981153dcf
Tony Gaetani <tonygaetani@users.noreply.github.com>
2017-10-19 15:52:13 -0500

    fix: more fn style param order and fn rename (#3)

--- lib/defer.ts
+++ lib/defer.ts
@@ -7,13 +7,13 @@ declare type Logger = {error: (data: any, msg: string) => any};
  * Returns an observable that will swallow (and optionally log) all errors thrown by a handler
  * and return either an empty Observable when an error occurs or the result of the handler
  *
- * @param {Function} handler: a function that is called and any errors thrown are safely swallowed
  * @param {Logger} log: (optional) a logger to log any errors
+ * @param {Function} handler: a function that is called and any errors thrown are safely swallowed
  * @return {Observable} an empty Observable when an error is thrown by the handler, otherwise
  * 							 the result of the handler function
- * @method safeDefer
+ * @method safe
  */
-export const safeDefer = R.curry((handler: () => any, log: Logger) => {
+export const safe = R.curry((log: Logger, handler: () => any) => {
 	return Rx.Observable.defer(handler)
 		.catch((err: Error, result: any) => {
 			if (log && log.error) {
--- test/defer.spec.ts
+++ test/defer.spec.ts
@@ -9,7 +9,7 @@ import * as R from 'ramda';
 const chance = new Chance();
 
 describe('defer', () => {
-	context('safeDefer', () => {
+	context('safe', () => {
 		it('should never fail', () => {
 			const input = R.range(1, chance.natural({min: 5, max: 100}));
 			const idx = chance.natural({min: 0, max: input.length - 1});
@@ -21,8 +21,9 @@ describe('defer', () => {
 				Rx.Observable.if(() => !x, Rx.Observable.throw(new Error()), Rx.Observable.of(x))
 			);
 			const log = {error: sinon.spy()};
+			const safeDefer = defer.safe(log);
 			const source = Rx.Observable.from(input)
-			.concatMap((v) => defer.safeDefer(() => handler(v), log));
+			.concatMap((v) => safeDefer(() => handler(v)));
 
 			return new Promise((resolve, reject) => {
 				source.subscribe(() => null, reject, () => {
