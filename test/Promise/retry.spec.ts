ca7a77f0db245e19ff90ea53a6dce0b79a00de80
David Durschlag <ddurschlag@6river.com>
2018-04-23 17:10:01 -0400

    fix: factor common code out of unit tests.

--- test/Promise/retry.spec.ts
+++ test/Promise/retry.spec.ts
@@ -2,72 +2,60 @@ import {assert} from 'chai';
 import {delay, retry} from '../../lib/Promise';
 
 describe('retry', function() {
-	it('succeeds early on success', async function() {
+	async function getOutcomeForFunction(f: () => Promise<number>, retries: number = 3) {
 		let errCount = 0;
+		let finalErr: Error|undefined;
+		let result: number|undefined;
 		const incErr = () => { errCount++; };
-		const f = async function() {
+		try {
+			result = await retry(f, retries, incErr);
+		} catch ( err ) {
+			finalErr = err;
+		}
+		return {result, finalErr, errCount};
+	}
+
+	it('succeeds early on success', async function() {
+		const {result, finalErr, errCount} = await getOutcomeForFunction(async function() {
 			return 1;
-		};
-		const result = await retry(f, 3, incErr);
+		});
 		assert.equal(result, 1);
 		assert.equal(errCount, 0);
 	});
 
 	it('succeeds on single-run success', async function() {
-		let errCount = 0;
-		const incErr = () => { errCount++; };
-		const f = async function() {
+		const {result, finalErr, errCount} = await getOutcomeForFunction(async function() {
 			return 1;
-		};
-		const result = await retry(f, 1, incErr);
+		}, 1);
 		assert.equal(result, 1);
 		assert.equal(errCount, 0);
 	});
 
 	it('fails on single-run failure', async function() {
-		let errCount = 0;
-		let finalErr: Error|undefined;
-		const incErr = () => { errCount++; };
-		const f = async function() {
+		const {result, finalErr, errCount} = await getOutcomeForFunction(async function() {
 			throw new Error(':(');
-		};
-		try {
-			await retry(f, 1, incErr);
-		} catch ( err ) {
-			finalErr = err;
-		}
+		}, 1);
 		assert.equal(finalErr.message, ':(');
 		assert.equal(errCount, 0);
 	});
 
 	it('succeeds early on success after failure', async function() {
-		let errCount = 0;
-		const incErr = () => { errCount++; };
 		let ranOnce = false;
-		const f = async function() {
+		const {result, finalErr, errCount} = await getOutcomeForFunction(async function() {
 			if ( !ranOnce ) {
 				ranOnce = true;
 				throw new Error(':(');
 			}
 			return 1;
-		};
-		const result = await retry(f, 3, incErr);
+		});
 		assert.equal(result, 1);
 		assert.equal(errCount, 1);
 	});
 
 	it('fails on continued failure', async function() {
-		let errCount = 0;
-		let finalErr: Error|undefined;
-		const incErr = () => { errCount++; };
-		const f = async function() {
+		const {result, finalErr, errCount} = await getOutcomeForFunction(async function() {
 			throw new Error(':(');
-		};
-		try {
-			await retry(f, 3, incErr);
-		} catch ( err ) {
-			finalErr = err;
-		}
+		});
 		assert.equal(finalErr.message, ':(');
 		assert.equal(errCount, 2);
 	});
